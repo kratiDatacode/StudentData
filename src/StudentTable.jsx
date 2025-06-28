6beaed598c077b5716cb491062e9f87dbd616368
kratiDatacode <krati.datacode@gmail.com>
2025-06-28 20:19:34 +0530

    add useEffect and useState Hook to fetch and display data from api

--- src/StudentTable.jsx
+++ src/StudentTable.jsx
@@ -1,38 +1,64 @@
-import {Link} from "react-router"
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
 const StudentTable = () => {
-  
-  return(
+  const [students, setStudents] = useState("");
+
+  //Fetching Students on Mount
+  useEffect(() => {
+    fetch("http://localhost:8000/students")
+      .then((res) => res.json()) //Promises
+      .then((data) => {
+        setStudents(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
     <>
-     <h2>Student Records</h2>
-     <div className="table-conatiner">
-      <Link to="/student/create" className="btn btn-add">Add new Student</Link>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Place</th>
-            <th>Phone</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Krati</td>
-            <td>Indore</td>
-            <td>7987525391</td>
-            <td>
-              <a href="" className="btn btn-info">View</a>
-              <a href="" className="btn btn-primary">Edit</a>
-              <a href="" className="btn btn-danger">Delete</a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-     </div>
+      <h2>Student Records</h2>
+      <div className="table-conatiner">
+        <Link to="/student/create" className="btn btn-add">
+          Add new Student
+        </Link>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Place</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students &&
+              students.map((item) => (
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.place}</td>
+                  <td>{item.phone}</td>
+                  <td>
+                    <a href="" className="btn btn-info">
+                      View
+                    </a>
+                    <a href="" className="btn btn-primary">
+                      Edit
+                    </a>
+                    <a href="" className="btn btn-danger">
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default StudentTable;
